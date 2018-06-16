0096658f1f8e30eb343990c64967c0a22f747ab1
Zoldur <zoldur99@gmail.com>
2018-06-16 03:21:45 -0700

    Get IP

--- xsn_install.sh
+++ xsn_install.sh
@@ -13,7 +13,7 @@ COIN_PORT=62583
 RPC_PORT=51473
 
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 
 RED='\033[0;31m'
@@ -138,7 +138,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
